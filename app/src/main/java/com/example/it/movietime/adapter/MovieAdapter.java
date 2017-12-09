b667ba98fd74c35b743a54054147eabde68fc599
Mangpali <mangpali29@gmail,com>
2017-12-09 10:38:47 +0800

    add model and adapter

---
+++ app/src/main/java/com/example/it/movietime/adapter/MovieAdapter.java
@@ -0,0 +1,8 @@
+package com.example.it.movietime.adapter;
+
+/**
+ * Created by Mangpali on 12/9/2017.
+ */
+
+public class MovieAdapter {
+}
--- app/src/main/java/com/example/it/movietime/model/movie.java
+++
@@ -1,8 +0,0 @@
-package com.example.it.movietime.model;
-
-/**
- * Created by Mangpali on 12/9/2017.
- */
-
-public class movie {
-}
